39badfb7cfde83217af715e560a923250575dc29
mudasirhayat <mudasirhayat5@gmail.com>
2025-09-16 13:00:17 +0000

    fix: improve error handling

--- mesaky_backend/asgi.py
+++ mesaky_backend/asgi.py
@@ -14,6 +14,11 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.sessions import SessionMiddlewareStack
 from core.routing import websocket_urlpatterns
 
+application = ProtocolTypeRouter({
+    'websocket': SessionMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mesaky_backend.settings")
 
 application = ProtocolTypeRouter(
