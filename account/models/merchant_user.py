7f370c5fa69a4af5c58a8ee0b910c173bf0482c8
mudasirhayat <mudasirhayat5@gmail.com>
2025-10-09 05:00:17 +0000

    refactor: improve code readability

--- account/models/merchant_user.py
+++ account/models/merchant_user.py
@@ -4,10 +4,9 @@ from .base_user import CustomUserManager, AbstractCustomUser
 try:
     # Your code here
 except Exception as e:
-    print(f"An error occurred: {e}")
-
+print(f"An error occurred: {e}")
 
 class User(AbstractCustomUser):
-  phone = models.CharField(max_length=255, null=True, blank=True)
+    phone = models.CharField(max_length=255, null=True, blank=True)
 
   objects = CustomUserManager()
