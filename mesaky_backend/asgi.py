a9fb1c55c436f12e575873428d6dc8e8e1723bcd
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-07 09:00:14 +0000

    fix: improve error handling

--- mesaky_backend/asgi.py
+++ mesaky_backend/asgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
+import logging
+
+logging.basicConfig(level=logging.ERROR)
 from channels.sessions import SessionMiddlewareStack
 from core.routing import websocket_urlpatterns
 
