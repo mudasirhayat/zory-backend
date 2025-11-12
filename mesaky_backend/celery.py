0c55c2cfdacb227012671d48a9f13260dc5d0183
mudasirhayat <mudasirhayat5@gmail.com>
2025-11-12 05:00:13 +0000

    fix: improve error handling

--- mesaky_backend/celery.py
+++ mesaky_backend/celery.py
@@ -1,10 +1,12 @@
 import os
+import os
 from celery import Celery
 
-# Set default Django settings module for Celery
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mesaky_backend.settings")
-
-app = Celery("mesaky_backend")
+try:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mesaky_backend.settings")
+    app = Celery("mesaky_backend")
+except Exception as e:
+    print(f"An
 
 # Load task modules from all registered Django app configs.
 app.config_from_object("django.conf:settings", namespace="CELERY")
