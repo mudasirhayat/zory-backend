7eb379e33254b2b475fa37ae451880310ad4c2a7
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-04 09:00:13 +0000

    fix: improve error handling

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token
     TokenRefreshView,
     TokenVerifyView,
 )
