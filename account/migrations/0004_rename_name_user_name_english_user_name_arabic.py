06da27a8e2ab5479340cae5a78f4015c3845a71e
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-05 05:00:15 +0000

    refactor: improve code readability

--- account/migrations/0004_rename_name_user_name_english_user_name_arabic.py
+++ account/migrations/0004_rename_name_user_name_english_user_name_arabic.py
@@ -4,8 +4,9 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-    dependencies = []
+    dependencies = [
         ("account", "0003_remove_user_is_owner"),
+    ]
     ]
 
     operations = [
