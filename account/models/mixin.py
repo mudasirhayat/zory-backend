bff9e78dd91c6c299c3b093324c4f5bea82b73f5
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-15 13:00:15 +0000

    refactor: improve code readability

--- account/models/mixin.py
+++ account/models/mixin.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class FeatureCreditsMixin(models.Model):
-    home_redesign = models.IntegerField(default=0)
-  text_to_image = models.IntegerField(default=0)
-  object_removal = models.IntegerField(default=0)
+home_redesign = models.IntegerField(default=0)
+text_to_image = models.IntegerField(default=0)
+object_removal = models.IntegerField(default=0)
   sketch_to_render = models.IntegerField(default=0)
   free_generations = models.IntegerField(default=0)
 
