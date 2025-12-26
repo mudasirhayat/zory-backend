9a8de26b48c4812603c4df778b29f4231ac5e19a
mudasirhayat <mudasirhayat5@gmail.com>
2025-12-26 17:00:15 +0000

    refactor: improve code readability

--- account/config/load_secrets.py
+++ account/config/load_secrets.py
@@ -6,8 +6,14 @@ def get_secret(secret_name: str, region_name: str = "me-central-1"):
 
     # Create a Secrets Manager client
     session = boto3.session.Session()
-client = session.client(service_name="secretsmanager", region_name=region_name)
-get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+client = session.client(
+    service_name="secretsmanager",
+    region_name=region_name
+)
+
+get_secret_value_response = client.get_secret_value(
+    SecretId=secret_name
+)
     except ClientError as e:
         # For a list of exceptions thrown, see
         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
