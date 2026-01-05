90871fb377ff350a63f4eeb8d228bc07b05e4e75
mudasirhayat <mudasirhayat5@gmail.com>
2026-01-05 13:00:15 +0000

    refactor: improve code readability

--- mesaky_backend/celery.py
+++ mesaky_backend/celery.py
@@ -6,8 +6,6 @@ try:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mesaky_backend.settings")
     app = Celery("mesaky_backend")
 except Exception as e:
-    print(f"An error occurred: {e}")
-    app.config_from_object("django.conf:settings", namespace="CELERY")
-
-# Autodiscover tasks from installed Django apps
+print(f"An error occurred: {e}")
+app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
