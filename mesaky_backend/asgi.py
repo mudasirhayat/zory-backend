2674573e48d343c709e223bf21fb805f2eb3f5bd
mudasirhayat <mudasirhayat5@gmail.com>
2026-02-15 09:00:16 +0000

    refactor: improve code readability

--- mesaky_backend/asgi.py
+++ mesaky_backend/asgi.py
@@ -1,9 +1,10 @@
 """
-ASGI config for mesaky_backend project.
+import os
+from django.core.asgi import get_asgi_application
 
-It exposes the ASGI callable as a module-level variable named ``application``.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mesaky_backend.settings')
 
-For more information on this file, see
+application = get_asgi_application()
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
