c6df00d21d1d710de43004d38498ccb414bf40ef
mudasirhayat <mudasirhayat5@gmail.com>
2026-02-26 17:00:15 +0000

    fix: improve error handling

--- account/models/merchant_user.py
+++ account/models/merchant_user.py
@@ -4,8 +4,7 @@ from .base_user import CustomUserManager, AbstractCustomUser
 try:
     # code that may raise an exception
 except Exception as e:
-    print(f"An error occurred: {e}")
-
+    raise e from None
 class User(AbstractCustomUser):
     phone = models.CharField(max_length=255, null=True, blank=True)
     objects = CustomUserManager()
