1d9358d986d3761731e1905b9ca5e9d84b8d3eb7
mudasirhayat <mudasirhayat5@gmail.com>
2026-02-26 13:00:19 +0000

    refactor: improve code readability

--- account/models/mixin.py
+++ account/models/mixin.py
@@ -5,7 +5,7 @@ home_redesign = models.IntegerField(default=0)
 text_to_image = models.IntegerField(default=0)
 object_removal = models.IntegerField(default=0)
   sketch_to_render = models.IntegerField(default=0)
-  free_generations = models.IntegerField(default=0)
+free_generations = models.IntegerField(default=0)
 
-  class Meta:
+class Meta:
     abstract = True
