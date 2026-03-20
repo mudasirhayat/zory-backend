465a3054fc12023d5eda9592d7661649eb725861
mudasirhayat <mudasirhayat5@gmail.com>
2026-03-20 05:00:16 +0000

    refactor: improve code readability

--- account/authentication.py
+++ account/authentication.py
@@ -4,6 +4,7 @@ from rest_framework.exceptions import AuthenticationFailed
 
 from .models.dashboard_user import DashboardUser
 from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class CustomJWTAuthentication(JWTAuthentication):
   def authenticate(self, request):
