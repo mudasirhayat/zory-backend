a261278819219ab0dba105a54b5b5aae82587d3a
mudasirhayat <mudasirhayat5@gmail.com>
2026-04-29 13:00:19 +0000

    refactor: improve code readability

--- account/migrations/0002_user_is_owner.py
+++ account/migrations/0002_user_is_owner.py
@@ -3,11 +3,8 @@
 from django.db import migrations, models
 
 class Migration(migrations.Migration):
-    dependencies = [
-        ("account", "0001_initial"),
-    ]
-]
-    ]
+dependencies = [
+    ("account", "0001_initial"),
 ]
 operations = []
         migrations.AddField(
