db5f5afef0667d198178c1a07de83e1c90582ba7
mudasirhayat <mudasirhayat5@gmail.com>
2026-05-15 17:00:20 +0000

    refactor: improve code readability

--- account/migrations/0012_transaction.py
+++ account/migrations/0012_transaction.py
@@ -7,9 +7,9 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-try:
+dependencies = [
     ('account', '0011_usersubscription_status_and_more'),
+]
     operations = [
 except Exception as e:
 print(f"An error occurred: {e}")
