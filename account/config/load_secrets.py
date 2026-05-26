cae8d066735afbe8187e1ca94a3c3f6b87b87400
mudasirhayat <mudasirhayat5@gmail.com>
2026-05-26 09:00:18 +0000

    fix: improve error handling

--- account/config/load_secrets.py
+++ account/config/load_secrets.py
@@ -2,9 +2,11 @@ import boto3
 from botocore.exceptions import ClientError
 
 def get_secret(secret_name: str, region_name: str = "us-east-1"):
+try:
     session = boto3.session.Session(region_name=region_name)
-client = session.client(
-    service_name="secretsmanager",
+    client = session.client(service_name="secretsmanager")
+except Exception as e:
+    print(f"An error occurred: {e}")
     region_name=region_name
 )
 
